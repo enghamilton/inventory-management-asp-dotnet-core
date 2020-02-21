72f608b881c47c4bba5a06a27c6f9b4229b9e2d9
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-02-21 07:15:56 -0300

    Update Startup.cs

--- InventoryManagement/Startup.cs
+++ InventoryManagement/Startup.cs
@@ -33,6 +33,12 @@ namespace InventoryManagement
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO")))
+            {
+                Console.WriteLine("Use https redirection");
+                app.UseHttpsRedirection();
+            }
+            /*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,6 +47,7 @@ namespace InventoryManagement
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            */
             app.UseStaticFiles();
 
             app.UseRouting();
