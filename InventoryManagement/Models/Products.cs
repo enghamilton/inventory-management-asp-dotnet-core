ec6c6c3be1926abc58b1328b449c7277f1360884
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-02-26 10:23:42 -0300

    Update Products.cs

--- InventoryManagement/Models/Products.cs
+++ InventoryManagement/Models/Products.cs
@@ -5,12 +5,9 @@ namespace InventoryManagement.Models
 {
     public partial class Products
     {
-        public long ProductId { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public string Color { get; set; }
-        public decimal UnitPrice { get; set; }
-        public long AvailableQuantity { get; set; }
-        public DateTime CratedDate { get; set; }
+        public long id { get; set; }
+        public string username { get; set; }
+        public string phone { get; set; }
+        public string price { get; set; }
     }
 }
